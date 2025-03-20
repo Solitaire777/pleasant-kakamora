b3a8220c3bc4c4e92408164eceb8e49a616816f0
SEsqueda <esquedastacy@gmail.com>
2025-03-20 13:02:29 -0400

    Initial commit

---
+++ src/main/java/sfg/pleasantkakamora/PleasantKakamoraApplication.java
@@ -0,0 +1,13 @@
+package sfg.pleasantkakamora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleasantKakamoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PleasantKakamoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sfg/pleasantkakamora/PleasantKakamoraApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.pleasantkakamora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleasantKakamoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
